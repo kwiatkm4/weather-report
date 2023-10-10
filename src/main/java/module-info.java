5631218ab295ae06431953856c431143af297fa9
TPGPL <TPGPL@users.noreply.github.com>
2023-10-10 23:02:25 +0200

    Add full jackson package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module pl.edu.pw.mwo1 {
 
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
+    requires java.net.http;
+    requires com.fasterxml.jackson.annotation;
 
     opens pl.edu.pw.mwo1 to javafx.fxml;
     exports pl.edu.pw.mwo1;
