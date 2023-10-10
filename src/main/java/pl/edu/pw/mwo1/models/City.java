8dab6fc2c6f610bce1359568f4aaff0559f316df
TPGPL <TPGPL@users.noreply.github.com>
2023-10-10 23:32:15 +0200

    Ignore unused properties in models

--- src/main/java/pl/edu/pw/mwo1/models/City.java
+++ src/main/java/pl/edu/pw/mwo1/models/City.java
@@ -1,7 +1,9 @@
 package pl.edu.pw.mwo1.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class City{
     @JsonProperty("Key")
     public String key;
